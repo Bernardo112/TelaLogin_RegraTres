b5dd36cb0ecc49ecef2b344fb13821c2fac558bf
Bernardo112 <bernardo.cruz@etec.sp.gov.br>
2023-02-15 02:34:58 +0900

    Project's Initial Commit

---
+++ www/components/script.js
@@ -0,0 +1,21 @@
+// This is a JavaScript file
+
+$(document).on("click", "#entrar", function()
+{
+    navigator.notification.alert(
+    'Cadastro efetuado com sucesso',  // message
+    function returno(){}, // callback
+    'Cadastro de Usuário', //title
+    'Logar' //ButtonName
+    );
+});
+
+$(document).on("click", "#entrar", function()
+{
+    navigator.notification.alert(
+    'Cadastro efetuado com sucesso',  // message
+    function returno(){}, // callback
+    'Cadastro de Usuário', //title
+    'Logar' //ButtonName
+    );
+});
